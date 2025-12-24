cd1ef5f62c364aeb904312d5736fd7c3a7d0c376
alexzaignacio <alexzagayleadaoignacio@gmail.com>
2025-12-24 20:03:53 +0800

    Implement main method for hotel service requests
    Added main method to HotelApp for service requests.

--- src/hotel/HotelApp.java
+++ src/hotel/HotelApp.java
@@ -1,4 +1,16 @@
 package hotel;
 
 public class HotelApp {
+    public static void main(String[] args) {
+        FrontDesk frontDesk = new FrontDesk();
+
+        // Request valet service
+        System.out.println(frontDesk.requestValet("ABC123"));
+
+        // Request room cleaning
+        System.out.println(frontDesk.requestCleaning("101"));
+
+        // Request luggage carts
+        System.out.println(frontDesk.requestCart(2));
+    }
 }
