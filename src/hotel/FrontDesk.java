8ef0c80e2e1b6fc845959386b6f98d583712222d
alexzaignacio <alexzagayleadaoignacio@gmail.com>
2025-12-24 20:02:34 +0800

    Update FrontDesk.java

--- src/hotel/FrontDesk.java
+++ src/hotel/FrontDesk.java
@@ -1,4 +1,25 @@
 package hotel;
 
 public class FrontDesk {
+    private Valet valet;
+    private HouseKeeping housekeeping;
+    private Cart cart;
+
+    public FrontDesk() {
+        this.valet = new Valet();
+        this.housekeeping = new HouseKeeping();
+        this.cart = new Cart();
+    }
+
+    public String requestValet(String plateNumber) {
+        return valet.pickUpVehicle(plateNumber);
+    }
+
+    public String requestCleaning(String roomNumber) {
+        return housekeeping.cleanRoom(roomNumber);
+    }
+
+    public String requestCart(int numberOfCarts) {
+        return cart.requestCart(numberOfCarts);
+    }
 }
